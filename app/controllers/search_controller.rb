4d892e67d7c571e5904261e63c365f6c0c31e93f
Lena Nahrwold <lena.nahrwold@posteo.de>
2020-01-11 00:36:38 +0100

    Add dictionary data and search logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,56 @@
 class SearchController < ApplicationController
+
   def home
   end
+
+  def result
+    @search_term = params["search_term"]
+    @main_selection = get_main_selection_for(@search_term)
+    @sub_selection = get_sub_selection_for(@search_term)
+  end
+
+  def details
+    @word = params['word']
+    @main_selection = get_entry_for(@word)
+    @sub_selection = get_sub_selection_for(@word)
+  end
+
+  private
+
+  def get_entry_for(search_term)
+    main_file = File.open("data/haupteintraege.json")
+    main_entries = JSON.load(main_file)
+    english_selection = main_entries.select do |entry|
+      entry["englisch"].downcase == search_term.downcase
+    end
+    japanese_selection = main_entries.select do |entry|
+      entry["japanisch"].downcase == search_term.downcase
+    end
+    return english_selection + japanese_selection
+  end
+  
+  def get_main_selection_for(search_term)
+    main_file = File.open("data/haupteintraege.json")
+    main_entries = JSON.load(main_file)
+    english_selection = main_entries.select do |entry|
+      entry["englisch"].downcase.include?(search_term.downcase)
+    end
+    japanese_selection = main_entries.select do |entry|
+      entry["japanisch"].downcase.include?(search_term.downcase)
+    end
+    return english_selection + japanese_selection
+  end
+  
+  def get_sub_selection_for(search_term)
+    sub_file = File.open("data/subeintraege.json")
+    sub_entries = JSON.load(sub_file)
+    sub_selection = []
+    search_term.each_char do |character|
+      sub_selection += sub_entries.select do |entry|
+        entry["japanisch"].include?(character)
+      end
+    end
+    return sub_selection
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'search#home'
+  
+  match '/search', to: 'search#home', via: :get
+  match '/search', to: 'search#result', via: :post
+
+  match '/details/:word', to: 'search#details', via: :get, as: :details
 
   devise_for :users
   resources :users
-  
+
 end
