1aa53d864b86d6503bce3269ab4417439459cfef
Lena Nahrwold <lena.nahrwold@posteo.de>
2020-04-11 20:31:46 +0200

    Search data in MainEntry and SubEntry database and link to external websites

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
 
+  # https://melvinchng.github.io/rails/SearchFeature.html#43-adding-a-simple-search-feature
+
   def home
   end
 
@@ -18,37 +20,21 @@ class SearchController < ApplicationController
   private
 
   def get_entry_for(search_term)
-    main_file = File.open("data/haupteintraege.json")
-    main_entries = JSON.load(main_file)
-    english_selection = main_entries.select do |entry|
-      entry["englisch"].downcase == search_term.downcase
-    end
-    japanese_selection = main_entries.select do |entry|
-      entry["japanisch"].downcase == search_term.downcase
-    end
-    return english_selection + japanese_selection
+    japanese_selection = MainEntry.all.where("japanese LIKE :search", search: search_term)
+    return japanese_selection
+
   end
-  
+
   def get_main_selection_for(search_term)
-    main_file = File.open("data/haupteintraege.json")
-    main_entries = JSON.load(main_file)
-    english_selection = main_entries.select do |entry|
-      entry["englisch"].downcase.include?(search_term.downcase)
-    end
-    japanese_selection = main_entries.select do |entry|
-      entry["japanisch"].downcase.include?(search_term.downcase)
-    end
+    japanese_selection = MainEntry.all.where("japanese LIKE :search", search: search_term)
+    english_selection = MainEntry.all.where("lower(english) LIKE :search", search: search_term.downcase)
     return english_selection + japanese_selection
   end
-  
+
   def get_sub_selection_for(search_term)
-    sub_file = File.open("data/subeintraege.json")
-    sub_entries = JSON.load(sub_file)
     sub_selection = []
     search_term.each_char do |character|
-      sub_selection += sub_entries.select do |entry|
-        entry["japanisch"].include?(character)
-      end
+      sub_selection += SubEntry.all.where("japanese LIKE :search", search: character)
     end
     return sub_selection
   end
