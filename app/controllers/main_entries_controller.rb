1ea4257e0c6eee522b1535012e881c966521eefc
Lena Nahrwold <lena.nahrwold@posteo.de>
2020-04-17 16:17:26 +0200

    Allow to make changes to online dictionary entries on main entries for admin and vip

--- app/controllers/main_entries_controller.rb
+++ app/controllers/main_entries_controller.rb
@@ -5,9 +5,9 @@ class MainEntriesController < ApplicationController
     def update
       @main_entry = MainEntry.find(params[:id])
       if @main_entry.update_attributes(secure_params)
-        redirect_to details_path(@main_entry.japanese), :notice => "Comment saved."
+        redirect_to details_path(@main_entry.japanese), :notice => "Changes saved."
       else
-        redirect_to details_path(@main_entry.japanese), :alert => "Unable to save comment."
+        redirect_to details_path(@main_entry.japanese), :alert => "Unable to save changes."
       end
     end
 
